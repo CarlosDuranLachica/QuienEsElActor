90acd416ae8911d24ae675d1e6879c8029853989
carlos <neardash@gmail.com>
2021-01-16 03:03:05 -0700

    Se agrego diseño responsive

--- src/componets/Details/Index.js
+++ src/componets/Details/Index.js
@@ -1,6 +1,6 @@
-import React from 'react'
+import React from "react";
 
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 import "./Style.css";
 
@@ -8,69 +8,88 @@ import { useSelector, useDispatch } from "react-redux";
 
 import { BorrarTheMovieAccion } from "../Redux/Reducers/TheMovieDB";
 
-import { Button } from 'antd';
-import { ArrowLeftOutlined } from '@ant-design/icons';
+import { Button } from "antd";
+import { ArrowLeftOutlined } from "@ant-design/icons";
 
-const Index = () => {   
-    
-    const dispatch = useDispatch();
-    
-    const DataDetails = useSelector((store) => store.TheMoviesData.Details);
-    console.log(DataDetails);
+const Index = () => {
+  const dispatch = useDispatch();
 
-    return (
-        <div className="Details">
-            <nav>
-                <Link to="/Home" onClick={() => dispatch(BorrarTheMovieAccion())}>
-                    <Button type="primary" icon={<ArrowLeftOutlined />}>
-                        Regresar
-                    </Button>
-                </Link>
-            </nav>
-            <main>
-                <section className="Info">
-                    {DataDetails.map((item) => (
-                        <div className="Flex-column Center">
-                            <img src={"https://image.tmdb.org/t/p/w500" + item.profile_path + ""} alt="actor" width="150px" />
-                            <h3>{item.name}</h3>
-                            <Button danger>
-                                {item.gender === 1 ? "Mujer" : "Hombre"}
-                            </Button>
-                            <h5>Popularidad: {item.popularity}</h5>
+  const DataDetails = useSelector((store) => store.TheMoviesData.Details);
+  console.log(DataDetails);
+
+  return (
+    <div className="Details">
+      <div className="Container">
+        <nav>
+          <Link to="/Home" onClick={() => dispatch(BorrarTheMovieAccion())}>
+            <Button type="primary" icon={<ArrowLeftOutlined />}>
+              Regresar
+            </Button>
+          </Link>
+        </nav>
+        <main>
+          <section className="Info">
+            {DataDetails.map((item) => (
+              <div className="Flex-column Center">
+                <img
+                  src={
+                    "https://image.tmdb.org/t/p/w500" + item.profile_path + ""
+                  }
+                  alt="actor"
+                  width="150px"
+                />
+                <div className="Flex-column Center">
+                  <h3>{item.name}</h3>
+                  <Button danger>
+                    {item.gender === 1 ? "Mujer" : "Hombre"}
+                  </Button>
+                  <h5>Popularidad: {item.popularity}</h5>
+                </div>
+              </div>
+            ))}
+          </section>
+          <section className="Movies">
+            <div>
+              <h1 className="Title-section">Peliculas:</h1>
+            </div>
+            <div>
+              {DataDetails.map((item) => (
+                <>
+                  {item.known_for.map((movie) => (
+                    <>
+                      <div className="Flex-row Start-End" style={{flexWrap:"wrap"}}>
+                        <p className="Title">{movie.original_title}</p>
+                        <p>
+                          {movie.vote_average}/10{" "}
+                          <span className="Icon-Start">★</span>
+                        </p>
+                      </div>
+                      <div className="Flex-row Center Content-Movie">
+                        <img
+                          src={
+                            "https://image.tmdb.org/t/p/w500" +
+                            movie.poster_path +
+                            ""
+                          }
+                          alt="poster Peliculas"
+                        />
+                        <div className="Container-Overview">
+                          <p className="Overview">{movie.overview}</p>
+                          <p className="Fecha">
+                            <b>Fecha de estreno: {movie.release_date}</b>
+                          </p>
                         </div>
-                    ))}
-                </section>
-                <section className="Movies">
-                    <div>
-                        <h1 className="Title-section">Peliculas:</h1>
-                    </div>
-                    <div>
-                        {DataDetails.map((item) => (
-                            <>
-                                {item.known_for.map((movie) => (
-                                    <>
-                                        <div className="Flex-row Start-End">
-                                            <p className="Title">{movie.original_title}</p>
-                                            <p>{movie.vote_average}/10 <span className="Icon-Start">★</span></p>
-                                        </div>
-                                        <div className="Flex-row Center Content-Movie">
-                                            <img src={"https://image.tmdb.org/t/p/w500" + movie.poster_path + ""} alt="poster Peliculas" width="150px" />
-                                            <div>
-                                                <p className="Overview">{movie.overview}</p>
-                                                <p className="Fecha"><b>Fecha de estreno: {movie.release_date}</b></p>
-                                            </div>
-                                        </div>
-
-                                    </>
-                                ))}
-                            </>
-                        ))}
-                    </div>
-
-                </section>
-            </main>
-        </div>
-    )
-}
+                      </div>
+                    </>
+                  ))}
+                </>
+              ))}
+            </div>
+          </section>
+        </main>
+      </div>
+    </div>
+  );
+};
 
 export default Index;
