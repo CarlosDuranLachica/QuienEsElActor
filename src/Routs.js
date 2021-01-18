e2cd5f28d24a662ca7c5cd81c39c6cd46771d514
carlos <neardash@gmail.com>
2021-01-17 18:06:46 -0700

    reparando el reload en depliege

--- src/Routs.js
+++ src/Routs.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import {BrowserRouter, Route, Redirect, Switch} from "react-router-dom"
+import { Route, Redirect, Switch} from "react-router-dom"
 
 /* Vistas */
 import Home from "./componets/Home/Index";
@@ -8,12 +8,10 @@ import Details from "./componets/Details/Index";
 
 export default function Routs() {
     return (
-        <BrowserRouter>
             <Switch>
                 <Route exact path="/Home" component={Home}/>
                 <Route exact path="/Details" component={Details}/>
                 <Redirect from="/" to="/Home"/>
             </Switch>
-        </BrowserRouter>
     )
 }
